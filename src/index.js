a609f8ab44e7bbd5cb0a6af311a75ebf191bd977
JaneL <juyeonsh412@gmail.com>
2020-10-27 00:34:52 +0900

    section4_create weatherDisplay using functional Components

--- src/index.js
+++ src/index.js
@@ -1,47 +1,18 @@
 // Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
-import ApprovalCard from './ApprovalCard';
 
 // Create a react component
 const App = () => {
-  return (
-      <div className="ui container comments">
-        <ApprovalCard>
-          <div>
-            <h4>Warning!</h4>
-            Are you sure you want to do this?
-          </div>
-        </ApprovalCard>
-
-        <ApprovalCard>
-          <CommentDetail 
-            author="Sam" 
-            timeAgo="Today at 4:45PM" 
-            content="Nice blog Post" 
-            avatar={faker.image.avatar()}
-          />
-        </ApprovalCard>
-        <ApprovalCard>
-          <CommentDetail 
-            author="Alex" 
-            timeAgo="Today at 2:00AM" 
-            content="I like the subject" 
-            avatar={faker.image.avatar()}
-          />
-        </ApprovalCard>
-        <ApprovalCard>
-          <CommentDetail 
-            author="Jane" 
-            timeAgo="Yesterday at 5:25PM" 
-            content="i like singing" 
-            avatar={faker.image.avatar()}
-          />
-        </ApprovalCard>
-    </div>
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (err) => console.log(err)
   );
+  return (
+    <div>
+    Latitude: 
+    </div>
+  )
 };
 
 // Take the react component and show it on the screen
